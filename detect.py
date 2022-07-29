16562013d50621839d5189c0c10d685d98b4e189
GuoHaoLan <coopticks@hotmail.com>
2022-07-29 13:51:09 +0800

    Push

--- detect.py
+++ detect.py
@@ -11,29 +11,25 @@ from autosub.constants import (
 import json
 import time
 
-async def getGoogle(bdata):
-    timeout = aiohttp.ClientTimeout(total=300)
-    headers = {"Content-Type": "audio/l16; rate=16000;"}
-    async with aiohttp.ClientSession() as session:   
-        for _ in range(5):
-            try:
-                async with session.post(GOOGLE_SPEECH_API_URL.format(lang="ja", key=GOOGLE_SPEECH_API_KEY), data=v, headers=headers, timeout=timeout) as resp:
-                    res = await resp.text()
-                    for line in res.split("\n"):
-                        line = json.loads(line)
-                        line = line['result'][0]['alternative'][0]['transcript']
-                        return line[:1].upper() + line[1:]
-            except:
-                continue
+
 async def main():
     regions, voiced = getSlices("../GVRD-94/1.wav")
     trans = []
-
-    for v in voiced:
-        i = await getGoogle(v)
-        if i:
-            print(i)
-            trans.append(i)
+    timeout = aiohttp.ClientTimeout(total=300)
+    headers = {"Content-Type": "audio/l16; rate=16000;"}
+    async with aiohttp.ClientSession() as session:   
+        for v in voiced:
+            for t in range(5):
+                try:
+                    async with session.post(GOOGLE_SPEECH_API_URL.format(lang="ja", key=GOOGLE_SPEECH_API_KEY), data=v, headers=headers, timeout=timeout) as resp:
+                        res = await resp.text()
+                        for line in res.split("\n"):
+                            line = json.loads(line)
+                            line = line['result'][0]['alternative'][0]['transcript']
+                            trans.append(line[:1].upper() + line[1:])
+                            break
+                except:
+                    print("Retry %d" % t)
 
     timed = [(r, t) for r, t in zip(regions, trans) if t]
     formatter = FORMATTERS.get("srt")
