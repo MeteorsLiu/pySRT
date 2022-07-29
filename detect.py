1b6707b3ee8fd883d873d05945b083e0ce121b2c
GuoHaoLan <coopticks@hotmail.com>
2022-07-29 13:42:57 +0800

    Push

--- detect.py
+++ detect.py
@@ -3,17 +3,38 @@ from autosub import SpeechRecognizer
 import os
 from multiprocessing import Pool, TimeoutError
 from autosub.formatters import FORMATTERS
+import aiohttp
+import asyncio
+from autosub.constants import (
+    LANGUAGE_CODES, GOOGLE_SPEECH_API_KEY, GOOGLE_SPEECH_API_URL,
+)
+import json
+import time
 
+async def getGoogle(bdata):
+    timeout = aiohttp.ClientTimeout(total=300)
+    headers = {"Content-Type": "audio/l16; rate=16000;"}
+    async with aiohttp.ClientSession() as session:   
+        for _ in range(5):
+            try:
+                async with session.post(GOOGLE_SPEECH_API_URL.format(lang="ja", key=GOOGLE_SPEECH_API_KEY), data=v, headers=headers, timeout=timeout) as resp:
+                    for line in resp.content.decode('utf-8').split("\n"):
+                        line = json.loads(line)
+                        line = line['result'][0]['alternative'][0]['transcript']
+                        return line[:1].upper() + line[1:]
+            except:
+                continue
 
 if __name__ == '__main__':
     recognizer = SpeechRecognizer()
     regions, voiced = getSlices("../GVRD-94/1.wav")
     trans = []
-    with Pool(os.cpu_count()+3) as pool:
-        for i in pool.imap(recognizer, voiced):
-            if not i is None:
-                trans.append(i)
-                print(i)
+
+    for v in voiced:
+        i = getGoogle(v)
+        if i:
+            print(i)
+            trans.append(i)
 
     timed = [(r, t) for r, t in zip(regions, trans) if t]
     formatter = FORMATTERS.get("srt")
